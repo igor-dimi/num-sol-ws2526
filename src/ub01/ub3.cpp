bebd2e77493f6e390a1ee796e2b77c140351113b
Igor Dimitrov <igor-dimi@protonmail.com>
2025-10-23 11:41:27 +0200

    ub1: programming assignment update to use templates

--- src/ub01/ub3.cpp
+++ src/ub01/ub3.cpp
@@ -3,54 +3,53 @@
 #include <iomanip>
 
 template <typename T>
-bool is_too_small(T x){
-    return T(1) + x == T(1);
+bool is_absorbed_by_one(T x) {
+    return static_cast<T>(1) + x == static_cast<T>(1);
 }
 
-// template <typename T>
-// void test_and_print(const char* label, T x) {
-//     const int prec = std::numeric_limits<T>::max_digits10; // safe round-trip
-//     T sum = T(1) + x;
+template <typename T>
+void run(const char* label) {
+    std::cout << "enter a number (" << label << "):\n> ";
 
-//     std::cout << label << ": 1 + "
-//               << std::setprecision(prec) << x
-//               << " == " << sum
-//               << (sum == T(1) ? " == 1" : " =/= 1")
-//               << '\n';
-// }
-
-int main() {
-    std::cout << "choose: " << std::endl;
-    std::cout << "1: single precision" << std::endl;
-    std::cout << "2: double precision" << std::endl;
-
-    int n;
-    std::cin >> n;
-
-    switch (n)
-    {
-    double x;
-    float xf;
-    case 1:
-        std::cout << "enter a number:" << std::endl;
-        std::cin >> x;
-        xf = static_cast<float>(x);
-        if (is_too_small<float>(xf)) {
-            std::cout << x << " is too small, i.e., 1 + x == 1" << std::endl;
-        } else std::cout << x << " is large enough, i.e. 1 + x != 1" << std::endl;
-        break;
-    case 2:
-        std::cout << "enter a number:" << std::endl;
-        std::cin >> x;
-        if (is_too_small<double>(x)) {
-            std::cout << x << " is too small, i.e., 1 + x == 1" << std::endl;
-        } else std::cout << x << " is large enough, i.e. 1 + x != 1" << std::endl;
-        break;
-    
-    default:
-        std::cout << "invalid input";
-        break;
+    T x{};
+    if (!(std::cin >> x)) {
+        std::cerr << "failed to read a " << label << " value.\n";
+        return;
     }
 
+    const int prec = std::numeric_limits<T>::max_digits10;
+    std::cout << std::setprecision(prec);
+
+    const T sum = static_cast<T>(1) + x;
+    const bool absorbed = is_absorbed_by_one(x);
+
+    if (is_absorbed_by_one(x)) {
+        std::cout << "x is too small, i.e: " << std::endl;
+        std::cout << "1 + " << x << " == " << sum;
+    } else {
+        std::cout << "x is large enough, i.e.: " << std::endl;
+        std::cout << "1 + " << x << " == " << sum << " =/= 1";
+    }
+    std::cout << std::endl;
+
+}
+
+int main() {
+    std::cout << "choose:\n"
+                 "1: single precision (float)\n"
+                 "2: double precision (double)\n> ";
+    int n{};
+    if (!(std::cin >> n)) {
+        std::cerr << "invalid menu input.\n";
+        return 1;
+    }
+
+    switch (n) {
+        case 1: { run<float>("float");  break; }
+        case 2: { run<double>("double"); break; }
+        default:
+            std::cout << "invalid choice\n";
+            break;
+    }
     return 0;
 }
