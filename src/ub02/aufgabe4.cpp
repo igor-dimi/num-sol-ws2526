de5e9ddbfa2ae1853a9e078b0e2a97337dbb9df1
Malte Herzog <malte.herzog.klemenz@gmail.com>
2025-10-29 18:41:01 +0100

    aufgabe 4 fertig

--- src/ub02/aufgabe4.cpp
+++ src/ub02/aufgabe4.cpp
@@ -6,18 +6,24 @@ double f(double x) {
         return 2*x;
     } else if (0.5 <= x && x <= 1) {
         return 2 - 2*x;
-    } else {
-        return 2;
+    }
+}
+
+double f_tilde(double x) {
+    if (0 <= x and x < 0.5) {
+        return 1.999999*x;
+    } else if (0.5 <= x && x <= 1) {
+        return  1.999999 * (1- x);
     }
 }
 
 
 int main(int argc, char const *argv[])
 {
-    double x_i = 0.76426;
+    double x_i = 0.01401;
     std::cout <<  x_i << std::endl;
     for (int i = 1; i <= 100; i++) {
-        x_i = f(x_i);
+        x_i = f_tilde(x_i);
         std::cout << x_i << std::endl;
     }
     return 0;
